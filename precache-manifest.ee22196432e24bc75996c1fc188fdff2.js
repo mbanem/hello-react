05afa2ead93902a58319a08bcbfb3cb7300d79db
mbanem <mili.comrunner@gmail.com>
2019-11-19 22:07:58 -0500

    Updates

---
+++ precache-manifest.ee22196432e24bc75996c1fc188fdff2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7837f53500c1a0f2a609ac67a8629f0f",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "a4ac605b8a3afdd26efb",
+    "url": "/hello-react/static/css/main.e077edc8.chunk.css"
+  },
+  {
+    "revision": "e3fb4519362aa1604b7b",
+    "url": "/hello-react/static/js/2.e2ff5c77.chunk.js"
+  },
+  {
+    "revision": "a4ac605b8a3afdd26efb",
+    "url": "/hello-react/static/js/main.8416d1fc.chunk.js"
+  },
+  {
+    "revision": "fe79d0baa1e1795a3c9b",
+    "url": "/hello-react/static/js/runtime-main.51b20af1.js"
+  }
+]);
